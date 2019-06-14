47da94b1224cb3c07412ff1a19663e7f6ab25c27
Robin Mitra <robinmitra1@gmail.com>
2019-06-14 22:07:54 +0100

    Update to v0.2.2.

--- Casks/gimme-quote-desktop.rb
+++ Casks/gimme-quote-desktop.rb
@@ -1,6 +1,6 @@
 cask 'gimme-quote-desktop' do
-  version '0.2.1'
-  sha256 'cc1093b86989eae3ab52e5b1e865e9d6de69f401efb1ffcd6ccbbf484cc55e77'
+  version '0.2.2'
+  sha256 '6181fa9ea0766d829672d93367cd00b3f88ed0dd5a550b3c8ba284294090e7f8'
 
   url "https://github.com/robinmitra/gimme-quote-desktop/releases/download/v#{version}/gimme-quote-#{version}-mac.zip"
   appcast 'https://github.com/robinmitra/gimme-quote-desktop/releases.atom'
